4c5e6112e297ac350039ae33961df05b461d5a6e
Eunji <eunjichong@gatech.edu>
2019-06-12 23:30:57 -0700

    initial commit

---
+++ demo.py
@@ -0,0 +1,173 @@
+import cv2, dlib
+import argparse, os, random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import datasets, transforms
+import pandas as pd
+import numpy as np
+from model import model_static
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from colour import Color
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--video', type=str, help='input video path. live cam is used when not specified')
+parser.add_argument('--face', type=str, help='face detection file path. dlib face detector is used when not specified')
+parser.add_argument('--model_weight', type=str, help='path to model weights file', default='data/model_weights.pkl')
+parser.add_argument('--jitter', type=int, help='jitter bbox n times, and average results', default=0)
+parser.add_argument('-save_vis', help='saves output as video', action='store_true')
+parser.add_argument('-save_text', help='saves output as text', action='store_true')
+parser.add_argument('-display_off', help='do not display frames', action='store_true')
+
+args = parser.parse_args()
+
+CNN_FACE_MODEL = 'data/mmod_human_face_detector.dat' # from http://dlib.net/files/mmod_human_face_detector.dat.bz2
+
+
+def bbox_jitter(bbox_left, bbox_top, bbox_right, bbox_bottom):
+    cx = (bbox_right+bbox_left)/2.0
+    cy = (bbox_bottom+bbox_top)/2.0
+    scale = random.uniform(0.8, 1.2)
+    bbox_right = (bbox_right-cx)*scale + cx
+    bbox_left = (bbox_left-cx)*scale + cx
+    bbox_top = (bbox_top-cy)*scale + cy
+    bbox_bottom = (bbox_bottom-cy)*scale + cy
+    return bbox_left, bbox_top, bbox_right, bbox_bottom
+
+
+def drawrect(drawcontext, xy, outline=None, width=0):
+    (x1, y1), (x2, y2) = xy
+    points = (x1, y1), (x2, y1), (x2, y2), (x1, y2), (x1, y1)
+    drawcontext.line(points, fill=outline, width=width)
+
+
+def run(video_path, face_path, model_weight, jitter, vis, display_off, save_text):
+    # set up vis settings
+    red = Color("red")
+    colors = list(red.range_to(Color("green"),10))
+    font = ImageFont.truetype("data/arial.ttf", 40)
+
+    # set up video source
+    if video_path is None:
+        cap = cv2.VideoCapture(0)
+        video_path = 'live.avi'
+    else:
+        cap = cv2.VideoCapture(video_path)
+
+    # set up output file
+    if save_text:
+        outtext_name = os.path.basename(video_path).replace('.avi','_output.txt')
+        f = open(outtext_name, "w")
+    if vis:
+        outvis_name = os.path.basename(video_path).replace('.avi','_output.avi')
+        imwidth = int(cap.get(3)); imheight = int(cap.get(4))
+        outvid = cv2.VideoWriter(outvis_name,cv2.VideoWriter_fourcc('M','J','P','G'), cap.get(5), (imwidth,imheight))
+
+    # set up face detection mode
+    if face_path is None:
+        facemode = 'DLIB'
+    else:
+        facemode = 'GIVEN'
+        column_names = ['frame', 'left', 'top', 'right', 'bottom']
+        df = pd.read_csv(face_path, names=column_names, index_col=0)
+        df['left'] -= (df['right']-df['left'])*0.2
+        df['right'] += (df['right']-df['left'])*0.2
+        df['top'] -= (df['bottom']-df['top'])*0.1
+        df['bottom'] += (df['bottom']-df['top'])*0.1
+        df['left'] = df['left'].astype('int')
+        df['top'] = df['top'].astype('int')
+        df['right'] = df['right'].astype('int')
+        df['bottom'] = df['bottom'].astype('int')
+
+    if (cap.isOpened()== False):
+        print("Error opening video stream or file")
+        exit()
+
+    cnn_face_detector = dlib.cnn_face_detection_model_v1(CNN_FACE_MODEL)
+    frame_cnt = 0
+
+    # set up data transformation
+    test_transforms = transforms.Compose([transforms.Resize(224), transforms.CenterCrop(224), transforms.ToTensor(),
+                                         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    # load model weights
+    model = model_static(model_weight)
+    model_dict = model.state_dict()
+    snapshot = torch.load(model_weight)
+    model_dict.update(snapshot)
+    model.load_state_dict(model_dict)
+
+    model.cuda()
+    model.train(False)
+
+    # video reading loop
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            height, width, channels = frame.shape
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            frame_cnt += 1
+            bbox = []
+            if facemode == 'DLIB':
+                dets = cnn_face_detector(frame, 1)
+                for d in dets:
+                    bbox.append([d.rect.left(),d.rect.top(),d.rect.right(),d.rect.bottom()])
+            elif facemode == 'GIVEN':
+                if frame_cnt in df.index:
+                    bbox.append([df.loc[frame_cnt,'left'],df.loc[frame_cnt,'top'],df.loc[frame_cnt,'right'],df.loc[frame_cnt,'bottom']])
+
+            frame = Image.fromarray(frame)
+            for b in bbox:
+                face = frame.crop((b))
+                img = test_transforms(face)
+                img.unsqueeze_(0)
+                if jitter > 0:
+                    for i in range(jitter):
+                        bj_left, bj_top, bj_right, bj_bottom = bbox_jitter(b[0], b[1], b[2], b[3])
+                        bj = [bj_left, bj_top, bj_right, bj_bottom]
+                        facej = frame.crop((bj))
+                        img_jittered = test_transforms(facej)
+                        img_jittered.unsqueeze_(0)
+                        img = torch.cat([img, img_jittered])
+
+                # forward pass
+                output = model(img.cuda())
+                if jitter > 0:
+                    output = torch.mean(output, 0)
+                score = F.sigmoid(output).item()
+
+                coloridx = min(int(round(score*10)),9)
+                draw = ImageDraw.Draw(frame)
+                drawrect(draw, [(b[0], b[1]), (b[2], b[3])], outline=colors[coloridx].hex, width=5)
+                draw.text((b[0],b[3]), str(round(score,2)), fill=(255,255,255,128), font=font)
+                if save_text:
+                    f.write("%d,%f\n"%(frame_cnt,score))
+
+            if not display_off:
+                frame = np.asarray(frame) # convert PIL image back to opencv format for faster display
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                cv2.imshow('',frame)
+                if vis:
+                    outvid.write(frame)
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord('q'):
+                    break
+        else:
+            break
+
+    if vis:
+        outvid.release()
+    if save_text:
+        f.close()
+    cap.release()
+    print 'DONE!'
+
+
+if __name__ == "__main__":
+    run(args.video, args.face, args.model_weight, args.jitter, args.save_vis, args.display_off, args.save_text)
---
+++ model.py
@@ -0,0 +1,121 @@
+import torch.nn as nn
+import torch
+import math
+
+
+def model_static(pretrained=False, **kwargs):
+    model = ResNet([3, 4, 6, 3], **kwargs)
+    if pretrained:
+        print 'loading saved model weights'
+        model_dict = model.state_dict()
+        snapshot = torch.load(pretrained)
+        snapshot = {k: v for k, v in snapshot.items() if k in model_dict}
+        model_dict.update(snapshot)
+        model.load_state_dict(model_dict)
+    return model
+
+
+class ResNet(nn.Module):
+    def __init__(self, layers):
+        super(ResNet, self).__init__()
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 3,
+                               bias = False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace = True)
+        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
+        self.layer1 = self._make_layer(64, layers[0])
+        self.layer2 = self._make_layer(128, layers[1], stride=2)
+        self.layer3 = self._make_layer(256, layers[2], stride=2)
+        self.layer4 = self._make_layer(512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride = 1)
+        self.fc_theta = nn.Linear(512 * Bottleneck.expansion, 34)
+        self.fc_phi = nn.Linear(512 * Bottleneck.expansion, 34)
+        self.fc_ec = nn.Linear(512 * Bottleneck.expansion, 1)
+        self.init_param()
+
+    def init_param(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2./n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.shape[0] * m.weight.shape[1]
+                m.weight.data.normal_(0, math.sqrt(2./n))
+                m.bias.data.zero_()
+
+    def _make_layer(self, planes, blocks, stride = 1):
+        downsample = None
+        layers = []
+
+        if stride != 1 or self.inplanes != planes * Bottleneck.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * Bottleneck.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * Bottleneck.expansion),
+                )
+
+        layers.append(Bottleneck(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * Bottleneck.expansion
+        for i in range(1, blocks):
+            layers.append(Bottleneck(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_ec(x)
+
+        return x
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
